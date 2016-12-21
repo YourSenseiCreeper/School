383d5086c6c557ea41f1a07c2b5dc1423f59706b
komp025 <komp025@10.0.0.95>
2016-12-21 09:35:32 +0100

    Konwerter z dziesiętnych na binarne

---
+++ src/day_6/Main.java
@@ -0,0 +1,25 @@
+package day_6;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int w = 0;
+		int konwerter = 2;
+		
+		Scanner sc = new Scanner(System.in);
+		msg("Podaj liczbe do przekonwertowania: ");
+		
+		String liczba = sc.next();
+		
+		
+		
+	}
+	
+	static void msg(String msg){
+		System.out.println(msg);
+	}
+
+}
