31f3094d5e82e2ee2743a6e48c240772e9a178b6
Szkola <komp025@10.0.0.87>
2016-12-22 09:23:06 +0100

    Update

---
+++ src/day_7_inNout/Main.java
@@ -0,0 +1,32 @@
+package day_7_inNout;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		PrintWriter out = null;
+		File f = new File("abc.txt");
+		try {
+			if(!f.exists()){
+				f.createNewFile();
+			}
+			out = new PrintWriter("abc.txt");
+			out.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("Podaj jakiœ tekst:\n");
+		System.out.println(f.getAbsolutePath());
+		out.append(sc.nextLine());
+		out.close();
+		
+	}
+
+}
---
+++ src/zegarek/Main.java
@@ -0,0 +1,15 @@
+package zegarek;
+
+import java.util.Date;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Date dt = new Date();
+		long t1 = dt.getTime();
+		System.out.println(t1);
+
+	}
+
+}
