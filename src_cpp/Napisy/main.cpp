2667ffe8cd7cb4697186ba95f7f7d91098cfb664
komp025 <komp025@10.0.0.87>
2017-01-04 09:40:54 +0100

    upd

--- src_cpp/Napisy/main.cpp
+++ src_cpp/Napisy/main.cpp
@@ -10,13 +10,21 @@ int main()
     int cnt = 0;
     int zoes = 0;
     int z,o = 0;
+    int sorter[15];
+
+    //Dodawanie wartosci
+    for(int i=2; i<15; i++){
+        sorter[i] = 0;
+    }
 
     plik.open("napisy.txt",ios::in);
 
-    while(plik.good()){
+    while(!plik.eof()){
         z = 0;
         o = 0;
+
         plik >> line;
+
         if(line.size()%2 == 0){
             cnt++;
         }
@@ -29,10 +37,17 @@ int main()
         if(z == o){
             zoes++;
         }
+
+        sorter[line.size()-1]++;
         cout << line << endl;
     }
+    plik.close();
     cout << "Liczba wierszy o parzystej d³ugosci to: " << cnt << endl;
     cout << "Liczba wierszy o tylu samych jedynkach i zerach: " << zoes << endl;
-    plik.close();
+
+    for(int i = 0; i<15; i++){
+        cout << i << "-znakowych: " << sorter[i] << endl;
+    }
+
     return 0;
 }
