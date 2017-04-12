3bfc2cd32f309eb0529e11a52c30d3a1fe5dcbb0
komp025 <komp025@DESKTOP-KVARNQE>
2017-04-12 09:43:18 +0200

    Bunt po Builderze

---
+++ src/bunt_po_builderze/Main.java
@@ -0,0 +1,14 @@
+package bunt_po_builderze;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		ProgramFrame pf = new ProgramFrame();
+		pf.setVisible(true);
+		pf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
